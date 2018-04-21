ae4f57ccd8bd075d70a1b677b695f47023b2a0f0
fatbros <02luvsic@gmail.com>
2018-04-21 17:18:10 +0900

    setting restful

--- server.py
+++ server.py
@@ -1,6 +1,23 @@
 from flask import Flask, render_template
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
+
+class HelloWorld(Resource):
+  def get(self):
+    return {'hello': 'world by get'}
+
+  def post(self):
+    return {'hello': 'world by post'}
+
+  def put(self):
+    return {'hello': 'world by put'}
+
+  def delete(self):
+    return {'hello': 'world by delete'}
+
+api.add_resource(HelloWorld, '/hello')
 
 @app.route('/')
 def hello():
