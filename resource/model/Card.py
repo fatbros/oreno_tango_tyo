b8068fae97799b25717f536c38ac55d5da142cfd
fatbros <02luvsic@gmail.com>
2018-04-30 00:23:24 +0900

    change model name cards to card

--- resource/model/Card.py
+++ resource/model/Card.py
@@ -2,34 +2,10 @@ from .Model import Model
 from flask_restful import abort
 
 
-class FormateCard():
-  def __init__(self, vo_data):
-    self.en_vo = vo_data['en_vo']
-    self.ja_vo = vo_data['ja_vo']
-
-  def get(self):
-    return {
-      'en_vo': self.en_vo,
-      'ja_vo': self.ja_vo
-    }
-
-
-def getFormatedCardsData(formated_cards):
-  tmp = []
-  for formated_card in formated_cards:
-    tmp.append(formated_card.get())
-  return tmp
-
-
-class CardsModel(Model):
+class CardModel(Model):
   def getAllCards(self, twitter_user_id):
     cards = self.db.cards.find({'twitter_user_id': twitter_user_id})
-
-    formated_cards = []
-    for card in cards:
-      formated_cards.append(FormateCard(card))
-
-    return getFormatedCardsData(formated_cards)
+    return self.formateMongoCursors(cards)
 
   def insertCard(self, en_vo, ja_vo, twitter_user_id):
     # pymongo error handle document
