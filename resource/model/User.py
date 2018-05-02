dd3da6a6bb2ef1c49ade3e1b06af71115b25264e
fatbros <02luvsic@gmail.com>
2018-05-02 22:53:24 +0900

    remove unnecessary comment

--- resource/model/User.py
+++ resource/model/User.py
@@ -3,11 +3,9 @@ from .Model import Model
 
 class UserModel(Model):
     def insertUser(self, account_setting):
-        # pymongo error handle document
-        # http://api.mongodb.com/python/current/api/pymongo/errors.html
         inserted_user = self.db.users.insert_one(account_setting)
 
-        return inserted_user.acknowledged
+        return inserted_user
 
     def getUser(self, user_id):
         get_user = self.db.users.find_one({'twitter_user_id': user_id})
