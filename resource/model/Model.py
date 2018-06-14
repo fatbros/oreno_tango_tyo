7f08239ca0a82dae29885a1fd77122b2408af41e
fatbros <02luvsic@gmail.com>
2018-06-14 23:00:26 +0900

    implements to get twitter access token

--- resource/model/Model.py
+++ resource/model/Model.py
@@ -2,7 +2,8 @@ from pymongo import MongoClient, errors
 
 
 class Model():
-    mongo_host = 'localhost'
+    # mongo_host = 'localhost'
+    mongo_host = 'mongodb://mongodb:27017/'
     mongo_port = 27017
 
     def __init__(self):
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from flask_dance.contrib.twitter import make_twitter_blueprint
 from flask_restful import Api
 
 from resource.api.cards import Cards
+from resource.api.twitter_request_token import TwitterRequestToken
 
 from resource.bp import index, logout
 
@@ -20,7 +21,8 @@ app.config.from_pyfile('config.py')
 
 # flask_restful setting
 api = Api(app)
-api.add_resource(Cards, '/cards')
+api.add_resource(Cards, '/api/cards')
+api.add_resource(TwitterRequestToken, '/api/v1/auth/twitter_request_token')
 
 # flask blueprint setting
 blueprint = make_twitter_blueprint(
